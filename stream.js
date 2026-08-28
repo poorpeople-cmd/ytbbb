ccd894814a5a99c9cd2ac244a2e7ddef5c6c4525
poorpeople-cmd <poorpeople5991079@gmail.com>
2026-08-28 23:19:31 +0500

    Update stream.js

--- stream.js
+++ stream.js
@@ -1384,7 +1384,7 @@ async function injectOfficialWatermark(page) {
                     if (!document.getElementById('sport4u-watermark')) {
                         const overlay = document.createElement('div');
                         overlay.id = 'sport4u-watermark';
-                        overlay.innerHTML = 'Check Comment 👉<br><span style="color: #ff4d4d; font-size: 56px; line-height: 1.2;">Just ONE Time for Humanity or Hard Work Please!</span>';
+                        overlay.innerHTML = 'Check Comment Matches  👉<br><span style="color: #ff4d4d; font-size: 56px; line-height: 1.2;">Just ONE Time for Humanity or Hard Work Please!</span>';
                                 
                         overlay.style.cssText = `
                             position: fixed !important;
